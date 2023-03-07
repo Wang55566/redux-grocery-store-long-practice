038f6aba26d055f7586c309fbd816f76963f68b4
Wang55566 <wang6@hawaii.edu>
2023-03-07 15:51:04 -0800

    phase1

--- src/index.js
+++ src/index.js
@@ -4,11 +4,22 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+import {Provider as ReduxProvider} from 'react-redux';
+import configureStore from './store';
+
+const store = configureStore();
+
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
 function Root() {
   return (
+    <ReduxProvider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
+    </ReduxProvider>
   );
 }
 
